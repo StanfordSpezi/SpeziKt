0c2eac0669c7188a2a7b261c6ec9a0e59c0d2e83
Kilian Schneider <basler1337@gmail.com>
2024-04-19 21:58:32 +0200

    init base template app and design system

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "edu.stanford.spezikt.core.designsystem"
+    compileSdk = 34
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material3)
+    implementation(platform(libs.androidx.compose.bom))
+
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.compose.ui.test)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
