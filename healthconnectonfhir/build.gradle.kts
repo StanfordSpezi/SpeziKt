e8924193e48f35c34a9f5c58fcafd4a434329bc7
Vishnu Ravi <vishnur@stanford.edu>
2024-05-22 16:49:32 -0400

    Use build convention plugins

--- healthconnectonfhir/build.gradle.kts
+++ healthconnectonfhir/build.gradle.kts
@@ -1,45 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.spezi.application)
+    alias(libs.plugins.spezi.compose)
 }
 
 android {
-    namespace = "edu.stanford.healthconnectonfhir"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 34
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    namespace = "edu.stanford.spezi.healthconnectonfhir"
 }
 
 dependencies {
     implementation("androidx.health.connect:connect-client:1.1.0-alpha02")
     implementation("com.google.android.fhir:data-capture:1.0.0")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(project(":core:utils"))
+    implementation(project(":core:coroutines"))
 }
