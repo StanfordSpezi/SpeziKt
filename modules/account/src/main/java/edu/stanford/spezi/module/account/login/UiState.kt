7cfb3893778ff15c1e4b770dad2c8749388740a9
Basler182 <showdialog@web.de>
2024-05-26 21:23:38 +0200

    added Account-Login: UiState
    Signed-off-by: Basler182 <showdialog@web.de>

---
+++ modules/account/src/main/java/edu/stanford/spezi/module/account/login/UiState.kt
@@ -0,0 +1,28 @@
+package edu.stanford.spezi.module.account.login
+
+import android.content.Context
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+
+data class UiState(
+    val password: String = "",
+    val email: String = "",
+    val passwordVisibility: Boolean = false,
+    val showProgress: Boolean = false,
+    val showFilterByAuthorizedAccounts: Boolean = true,
+    val googleIdTokenCredential: GoogleIdTokenCredential? = null
+)
+
+enum class TextFieldType {
+    PASSWORD, EMAIL
+}
+
+enum class NavigationTarget {
+    LOGIN, REGISTER
+}
+
+sealed interface Action {
+    data class TextFieldUpdate(val newValue: String, val type: TextFieldType) : Action
+    data object TogglePasswordVisibility : Action
+    data class NavigateToRegister(val type: NavigationTarget) : Action
+    data class GoogleSignIn(val context: Context) : Action
+}
