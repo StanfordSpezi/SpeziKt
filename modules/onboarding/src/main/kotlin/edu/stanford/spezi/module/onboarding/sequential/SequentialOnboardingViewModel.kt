f92bfc5a0f8a71581678e15dd315a24a2587d3b6
Basler182 <showdialog@web.de>
2024-05-26 17:11:13 +0200

    added Onboarding-Sequential: SequentialOnboardingViewModel + Test
    Signed-off-by: Basler182 <showdialog@web.de>

---
+++ modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/sequential/SequentialOnboardingViewModel.kt
@@ -0,0 +1,72 @@
+package edu.stanford.spezi.module.onboarding.sequential
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.stanford.spezi.core.navigation.NavigationEvent
+import edu.stanford.spezi.core.navigation.Navigator
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * ViewModel for the [edu.stanford.spezi.module.onboarding.sequential.SequentialOnboardingScreen].
+ */
+@HiltViewModel
+class SequentialOnboardingViewModel @Inject internal constructor(
+    private val navigator: Navigator,
+    private val repository: SequentialOnboardingRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(SequentialOnboardingUiState())
+    val uiState: StateFlow<SequentialOnboardingUiState> = _uiState
+
+    init {
+        fetchSteps()
+    }
+
+    private fun fetchSteps() {
+        viewModelScope.launch {
+            val steps = repository.getSteps()
+            _uiState.value = SequentialOnboardingUiState(steps = steps)
+        }
+    }
+
+    fun onAction(action: Action) {
+        val currentPage = when (action) {
+            is Action.UpdatePage -> {
+                when (action.event) {
+                    ButtonEvent.FORWARD -> {
+                        if (uiState.value.currentPage == uiState.value.pageCount - 1) {
+                            navigator.navigateTo(NavigationEvent.InvitationCodeScreen)
+                            return
+                        } else {
+                            uiState.value.currentPage + 1
+                        }
+                    }
+
+                    ButtonEvent.BACKWARD -> {
+                        if (uiState.value.currentPage == 0) {
+                            navigator.navigateTo(NavigationEvent.OnboardingScreen)
+                            return
+                        } else {
+                            uiState.value.currentPage - 1
+                        }
+                    }
+                }
+            }
+
+            is Action.SetPage -> action.page
+        }
+        _uiState.value = uiState.value.copy(currentPage = currentPage)
+    }
+}
+
+object SequentialOnboardingViewModelFactory {
+    fun create(
+        navigator: Navigator,
+        repository: SequentialOnboardingRepository
+    ): SequentialOnboardingViewModel {
+        return SequentialOnboardingViewModel(navigator, repository)
+    }
+}
---
+++ modules/onboarding/src/test/kotlin/edu/stanford/spezi/module/onboarding/sequential/SequentialOnboardingViewModelTest.kt
@@ -0,0 +1,67 @@
+package edu.stanford.spezi.module.onboarding.sequential
+
+import com.google.common.truth.Truth.assertThat
+import edu.stanford.spezi.core.navigation.Navigator
+import edu.stanford.spezi.core.testing.CoroutineTestRule
+import edu.stanford.spezi.core.testing.runTestUnconfined
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SequentialOnboardingViewModelTest {
+    private val navigator: Navigator = mockk(relaxed = true)
+    private val repository: SequentialOnboardingRepository = mockk(relaxed = true)
+
+    @get:Rule
+    val coroutineTestRule = CoroutineTestRule()
+
+    private lateinit var sequentialOnboardingViewModel: SequentialOnboardingViewModel
+
+    @Before
+    fun setup() = runBlocking {
+        coEvery { repository.getSteps() } returns listOf(
+            Step(title = "title", description = "description", icon = 0),
+            Step(title = "title", description = "description", icon = 0)
+        )
+
+        sequentialOnboardingViewModel =
+            SequentialOnboardingViewModelFactory.create(navigator, repository)
+    }
+
+    @Test
+    fun `it should fetch steps on init`() = runTestUnconfined {
+        // when
+        val uiState = sequentialOnboardingViewModel.uiState.first()
+
+        // then
+        verify { runBlocking { repository.getSteps() } }
+        assertThat(uiState.steps).isNotEmpty()
+    }
+
+    @Test
+    fun `it should start with first page`() = runTestUnconfined {
+        // when
+        val uiState = sequentialOnboardingViewModel.uiState.first()
+
+        // then
+        assertThat(uiState.currentPage).isEqualTo(0)
+    }
+
+    @Test
+    fun `it should update current page correctly`() = runTestUnconfined {
+        // given
+        val action = Action.UpdatePage(ButtonEvent.FORWARD)
+
+        // when
+        sequentialOnboardingViewModel.onAction(action)
+        val uiState = sequentialOnboardingViewModel.uiState.first()
+
+        // then
+        assertThat(uiState.currentPage).isEqualTo(1)
+    }
+}
