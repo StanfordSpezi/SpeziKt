5735270faee20c95e44bcb84db10858ecb06371f
Basler182 <showdialog@web.de>
2024-05-26 10:22:54 +0200

    added Onboarding-Sequential: Step
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/spezi/app/onboarding/DefaultSequentialOnboardingScreenRepository.kt
+++ app/src/main/kotlin/edu/stanford/spezi/app/onboarding/DefaultSequentialOnboardingScreenRepository.kt
@@ -10,29 +10,29 @@ class DefaultSequentialOnboardingScreenRepository : SequentialOnboardingScreenRe
     override suspend fun getSteps(): List<Step> {
         return listOf(
             Step(
-                "Pair with Devices",
-                "Pair with the provided weight scale and blood pressure cuff in Bluetooth settings.",
-                image = edu.stanford.spezi.core.design.R.drawable.ic_bluetooth
+                title = "Pair with Devices",
+                description = "Pair with the provided weight scale and blood pressure cuff in Bluetooth settings.",
+                icon = edu.stanford.spezi.core.design.R.drawable.ic_bluetooth
             ),
             Step(
-                "Record Health Data",
-                "Use the weight scale and blood pressure cuff to record health data in Heart Health.",
-                image = edu.stanford.spezi.core.design.R.drawable.ic_assignment
+                title = "Record Health Data",
+                description = "Use the weight scale and blood pressure cuff to record health data in Heart Health.",
+                icon = edu.stanford.spezi.core.design.R.drawable.ic_assignment
             ),
             Step(
-                "Tune Medications",
-                "See your medication dosage, schedule, and updates in Medications.",
-                image = edu.stanford.spezi.core.design.R.drawable.ic_medication
+                title = "Tune Medications",
+                description = "See your medication dosage, schedule, and updates in Medications.",
+                icon = edu.stanford.spezi.core.design.R.drawable.ic_medication
             ),
             Step(
                 title = "Summarize",
                 description = "Generate and export a full PDF health report in Health Summary.",
-                image = edu.stanford.spezi.core.design.R.drawable.ic_assignment
+                icon = edu.stanford.spezi.core.design.R.drawable.ic_assignment
             ),
             Step(
                 title = "Learn",
                 description = "Learn more about your medications and heart health in Education.",
-                image = edu.stanford.spezi.core.design.R.drawable.ic_school
+                icon = edu.stanford.spezi.core.design.R.drawable.ic_school
             )
         )
     }
