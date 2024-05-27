33205801582f48a559d7be9aa8b6ddc2fcabf50e
Vishnu Ravi <vishnur@stanford.edu>
2024-05-26 20:01:08 -0400

    Extract dependencies to version catalog

--- modules/healthconnectonfhir/build.gradle.kts
+++ modules/healthconnectonfhir/build.gradle.kts
@@ -7,9 +7,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.health.connect:connect-client:1.1.0-alpha02")
-    implementation("com.google.android.fhir:data-capture:1.0.0")
-
-    implementation(project(":core:utils"))
-    implementation(project(":core:coroutines"))
+    implementation(libs.androidx.health.connect.client)
+    implementation(libs.android.fhir.data.capture)
 }
