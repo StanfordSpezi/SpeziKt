771b795a32932f108bbc2cf87228dba3e59bddd9
Basler182 <showdialog@web.de>
2024-06-01 10:39:18 +0200

    used binds instead of providing whole object
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/spezi/app/onboarding/OnboardingModule.kt
+++ app/src/main/kotlin/edu/stanford/spezi/app/onboarding/OnboardingModule.kt
@@ -1,35 +1,34 @@
 package edu.stanford.spezi.app.onboarding
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import edu.stanford.spezi.core.coroutines.di.Dispatching
 import edu.stanford.spezi.module.onboarding.invitation.FirebaseInvitationAuthManager
 import edu.stanford.spezi.module.onboarding.invitation.InvitationAuthManager
 import edu.stanford.spezi.module.onboarding.onboarding.OnboardingRepository
 import edu.stanford.spezi.module.onboarding.sequential.SequentialOnboardingRepository
-import kotlinx.coroutines.CoroutineScope
-import javax.inject.Singleton
 
 /**
  *  A Dagger module that provides dependencies for the onboarding feature.
  */
 @Module
 @InstallIn(SingletonComponent::class)
-object OnboardingModule {
+class OnboardingModule {
 
-    @Provides
-    @Singleton
-    fun provideOnboardingRepository(
-        @Dispatching.IO ioCoroutineScope: CoroutineScope
-    ): OnboardingRepository {
-        return DefaultOnboardingRepository(ioCoroutineScope)
-    }
+    abstract class Bindings {
 
-    @Provides
-    fun provideInvitationAuthManager(): InvitationAuthManager {
-        return FirebaseInvitationAuthManager()
+
+        @Binds
+        abstract fun bindInvitationAuthManager(
+            firebaseInvitationAuthManager: FirebaseInvitationAuthManager
+        ): InvitationAuthManager
+
+        @Binds
+        abstract fun bindOnboardingRepository(
+            defaultOnboardingRepository: DefaultOnboardingRepository
+        ): OnboardingRepository
     }
 
     @Provides
