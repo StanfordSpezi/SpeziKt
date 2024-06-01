0af97a0846383d9d7650efb880c2e191f6c33fee
Basler182 <showdialog@web.de>
2024-06-01 10:38:53 +0200

    added run catching
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/spezi/app/onboarding/DefaultOnboardingRepository.kt
+++ app/src/main/kotlin/edu/stanford/spezi/app/onboarding/DefaultOnboardingRepository.kt
@@ -4,53 +4,44 @@ import edu.stanford.spezi.core.coroutines.di.Dispatching
 import edu.stanford.spezi.core.design.R
 import edu.stanford.spezi.module.onboarding.onboarding.Area
 import edu.stanford.spezi.module.onboarding.onboarding.OnboardingRepository
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DefaultOnboardingRepository @Inject constructor(
-    @Dispatching.IO private val scope: CoroutineScope,
+    @Dispatching.IO private val ioDispatcher: CoroutineDispatcher,
 ) : OnboardingRepository {
 
-    override suspend fun getAreas(): Result<List<Area>> = withContext(scope.coroutineContext) {
-        try {
-            Result.success(
-                listOf(
-                    Area(
-                        title = "Join the Study",
-                        iconId = R.drawable.ic_groups,
-                        description = "Connect to your study via an invitation code from the researchers."
-                    ),
-                    Area(
-                        title = "Complete Health Checks",
-                        iconId = R.drawable.ic_assignment,
-                        description = "Record and report health data automatically according to a schedule set by the research team."
-                    ),
-                    Area(
-                        title = "Visualize Data",
-                        iconId = R.drawable.ic_vital_signs,
-                        description = "Visualize your heart health progress throughout participation in the study."
-                    )
-                )
+    override suspend fun getAreas(): Result<List<Area>> = Result.success(
+        listOf(
+            Area(
+                title = "Join the Study",
+                iconId = R.drawable.ic_groups,
+                description = "Connect to your study via an invitation code from the researchers."
+            ),
+            Area(
+                title = "Complete Health Checks",
+                iconId = R.drawable.ic_assignment,
+                description = "Record and report health data automatically according to a schedule set by the research team."
+            ),
+            Area(
+                title = "Visualize Data",
+                iconId = R.drawable.ic_vital_signs,
+                description = "Visualize your heart health progress throughout participation in the study."
             )
-        } catch (e: Exception) {
-            Result.failure(e)
+        )
+    )
+
+
+    override suspend fun getTitle(): Result<String> = withContext(ioDispatcher) {
+        runCatching {
+            "Welcome to ENGAGE-HF"
         }
     }
 
-    override suspend fun getTitle(): Result<String> = withContext(scope.coroutineContext) {
-        try {
-            Result.success("Welcome to ENGAGE-HF")
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
-    override suspend fun getSubtitle(): Result<String> = withContext(scope.coroutineContext) {
-        try {
-            Result.success("Remote study participation made easy.")
-        } catch (e: Exception) {
-            Result.failure(e)
+    override suspend fun getSubtitle(): Result<String> = withContext(ioDispatcher) {
+        runCatching {
+            "Remote study participation made easy."
         }
     }
 }
