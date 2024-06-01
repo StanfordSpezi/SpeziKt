f99643427f9fb77e8a016356148444040ad9c6da
Basler182 <showdialog@web.de>
2024-06-01 11:41:29 +0200

    DI firebase functions and auth
    Signed-off-by: Basler182 <showdialog@web.de>

---
+++ app/src/main/kotlin/edu/stanford/spezi/app/onboarding/FirebaseModule.kt
@@ -0,0 +1,31 @@
+package edu.stanford.spezi.app.onboarding
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseModule {
+
+    private val host = "10.0.2.2"
+
+    @Provides
+    fun provideFirebaseFunctions(): FirebaseFunctions = FirebaseFunctions.getInstance().apply {
+        useEmulator(host, 5001)
+    }
+
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance().apply {
+        useEmulator(host, 9099)
+    }
+
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance().apply {
+        useEmulator(host, 8080)
+    }
+}
