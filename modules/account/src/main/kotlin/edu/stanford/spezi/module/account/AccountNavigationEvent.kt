96ca435e9480595f4ea2f66c979fa530b1931fa3
Basler182 <showdialog@web.de>
2024-06-09 12:08:17 +0200

    updated AccountNavigationEvent to take email and password from previous screen
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/account/src/main/kotlin/edu/stanford/spezi/module/account/AccountNavigationEvent.kt
+++ modules/account/src/main/kotlin/edu/stanford/spezi/module/account/AccountNavigationEvent.kt
@@ -3,6 +3,11 @@ package edu.stanford.spezi.module.account
 import edu.stanford.spezi.core.navigation.NavigationEvent
 
 sealed class AccountNavigationEvent : NavigationEvent {
-    data class RegisterScreen(val isGoogleSignIn: Boolean) : AccountNavigationEvent()
+    data class RegisterScreen(
+        val isGoogleSignUp: Boolean = false,
+        val email: String = "",
+        val password: String = "",
+    ) : AccountNavigationEvent()
+
     data class LoginScreen(val isAlreadyRegistered: Boolean) : AccountNavigationEvent()
 }
