a9e1205920905ad07c37cd0f738764ced2eea8ea
Basler182 <showdialog@web.de>
2024-06-23 12:01:30 +0200

    added auth state listener to directly navigate to home when logged in
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/MainActivityViewModel.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/MainActivityViewModel.kt
@@ -2,12 +2,15 @@ package edu.stanford.bdh.engagehf
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.stanford.bdh.engagehf.navigation.AppNavigationEvent
 import edu.stanford.spezi.core.logging.speziLogger
 import edu.stanford.spezi.core.navigation.NavigationEvent
 import edu.stanford.spezi.core.navigation.Navigator
 import edu.stanford.spezi.module.account.AccountEvents
+import edu.stanford.spezi.module.onboarding.OnboardingNavigationEvent
+import edu.stanford.spezi.module.onboarding.consent.PdfService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,22 +19,49 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(
     private val accountEvents: AccountEvents,
     private val navigator: Navigator,
+    private val firebaseAuth: FirebaseAuth,
+    private val pdfService: PdfService,
 ) : ViewModel() {
     private val logger by speziLogger()
 
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        val user = firebaseAuth.currentUser
+        user?.let {
+            if (!it.isAnonymous) {
+                // If the user is not anonymous, we can check if the PDF has been uploaded
+                viewModelScope.launch {
+                    if (pdfService.isPdfUploaded().isSuccess) {
+                        navigator.navigateTo(AppNavigationEvent.BluetoothScreen)
+                    } else {
+                        // User has to consent to the study before proceeding and upload the PDF
+                        navigator.navigateTo(OnboardingNavigationEvent.ConsentScreen)
+                    }
+                }
+            }
+            // If the user is anonymous, we don't need to check for the PDF and user stays on the start screen
+        }
+    }
+
     init {
+        firebaseAuth.addAuthStateListener(authStateListener)
         viewModelScope.launch {
             accountEvents.events.collect { event ->
                 when (event) {
                     is AccountEvents.Event.SignUpSuccess, AccountEvents.Event.SignInSuccess -> {
                         navigator.navigateTo(AppNavigationEvent.BluetoothScreen)
                     }
+
                     else -> {
                         logger.i { "Ignoring registration event: $event" }
                     }
                 }
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        firebaseAuth.removeAuthStateListener(authStateListener)
     }
 
     fun getNavigationEvents(): Flow<NavigationEvent> = navigator.events
--- modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/FirebasePdfService.kt
+++ modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/FirebasePdfService.kt
@@ -2,6 +2,7 @@ package edu.stanford.spezi.module.onboarding.consent
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageException
 import edu.stanford.spezi.core.coroutines.di.Dispatching
 import edu.stanford.spezi.core.logging.speziLogger
 import kotlinx.coroutines.CoroutineDispatcher
@@ -10,21 +11,40 @@ import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
 import javax.inject.Inject
 
-internal class FirebasePdfService @Inject internal constructor(
+class FirebasePdfService @Inject internal constructor(
     private val firebaseStorage: FirebaseStorage,
     private val firebaseAuth: FirebaseAuth,
     @Dispatching.IO private val ioDispatcher: CoroutineDispatcher,
 ) : PdfService {
     private val logger by speziLogger()
 
-    override suspend fun uploadPdf(pdfBytes: ByteArray): Result<Boolean> = withContext(ioDispatcher) {
+    override suspend fun uploadPdf(pdfBytes: ByteArray): Result<Boolean> =
+        withContext(ioDispatcher) {
+            runCatching {
+                firebaseAuth.uid?.let { uid ->
+                    val inputStream = ByteArrayInputStream(pdfBytes)
+                    logger.i { "Uploading file to Firebase Storage" }
+                    firebaseStorage.getReference("users/$uid/signature.pdf")
+                        .putStream(inputStream).await().task.isSuccessful
+                } ?: false
+            }
+        }
+
+    override suspend fun isPdfUploaded(): Result<Boolean> = withContext(ioDispatcher) {
         runCatching {
-            firebaseAuth.uid?.let { uid ->
-                val inputStream = ByteArrayInputStream(pdfBytes)
-                logger.i { "Uploading file to Firebase Storage" }
-                firebaseStorage.getReference("users/$uid/signature.pdf")
-                    .putStream(inputStream).await().task.isSuccessful
-            } ?: false
+            val uid = firebaseAuth.uid ?: return@runCatching false
+            val reference = firebaseStorage.getReference("users/$uid/signature.pdf")
+            try {
+                reference.metadata.await()
+                true
+            } catch (e: StorageException) {
+                if (e.errorCode == StorageException.ERROR_OBJECT_NOT_FOUND) {
+                    false
+                } else {
+                    logger.e { "Error while checking if file is uploaded to Firebase Storage" }
+                    false
+                }
+            }
         }
     }
 }
