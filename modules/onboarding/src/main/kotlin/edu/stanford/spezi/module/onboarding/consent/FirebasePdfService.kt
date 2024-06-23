c1fe605fe681732637ad1f6f7c775304f816546a
Basler182 <showdialog@web.de>
2024-06-23 12:09:01 +0200

    added auth state listener to directly navigate to home when logged in
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/FirebasePdfService.kt
+++ modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/FirebasePdfService.kt
@@ -2,6 +2,7 @@ package edu.stanford.spezi.module.onboarding.consent
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageException
 import edu.stanford.spezi.core.coroutines.di.Dispatching
 import edu.stanford.spezi.core.logging.speziLogger
 import kotlinx.coroutines.CoroutineDispatcher
@@ -17,14 +18,33 @@ internal class FirebasePdfService @Inject internal constructor(
 ) : PdfService {
     private val logger by speziLogger()
 
-    override suspend fun uploadPdf(pdfBytes: ByteArray): Result<Boolean> = withContext(ioDispatcher) {
+    override suspend fun uploadPdf(pdfBytes: ByteArray): Result<Boolean> =
+        withContext(ioDispatcher) {
+            runCatching {
+                firebaseAuth.uid?.let { uid ->
+                    val inputStream = ByteArrayInputStream(pdfBytes)
+                    logger.i { "Uploading file to Firebase Storage" }
+                    firebaseStorage.getReference("users/$uid/signature.pdf")
+                        .putStream(inputStream).await().task.isSuccessful
+                } ?: false
+            }
+        }
+
+    override suspend fun isPdfUploaded(): Result<Boolean> = withContext(ioDispatcher) {
         runCatching {
-            firebaseAuth.uid?.let { uid ->
-                val inputStream = ByteArrayInputStream(pdfBytes)
-                logger.i { "Uploading file to Firebase Storage" }
-                firebaseStorage.getReference("users/$uid/signature.pdf")
-                    .putStream(inputStream).await().task.isSuccessful
-            } ?: false
+            val uid = firebaseAuth.uid ?: return@runCatching false
+            val reference = firebaseStorage.getReference("users/$uid/signature.pdf")
+            try {
+                reference.metadata.await()
+                true
+            } catch (e: StorageException) {
+                if (e.errorCode == StorageException.ERROR_OBJECT_NOT_FOUND) {
+                    false
+                } else {
+                    logger.e { "Error while checking if file is uploaded to Firebase Storage" }
+                    false
+                }
+            }
         }
     }
 }
--- modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/PdfService.kt
+++ modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/PdfService.kt
@@ -3,4 +3,6 @@ package edu.stanford.spezi.module.onboarding.consent
 interface PdfService {
 
     suspend fun uploadPdf(pdfBytes: ByteArray): Result<Boolean>
+
+    suspend fun isPdfUploaded(): Result<Boolean>
 }
