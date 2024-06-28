e41e638628cda84529b603d891dedcfa3636f5ba
Basler182 <showdialog@web.de>
2024-06-28 23:11:07 +0200

    added app screen navigation event
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/navigation/AppNavigationEvent.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/navigation/AppNavigationEvent.kt
@@ -3,5 +3,5 @@ package edu.stanford.bdh.engagehf.navigation
 import edu.stanford.spezi.core.navigation.NavigationEvent
 
 sealed interface AppNavigationEvent : NavigationEvent {
-    data object BluetoothScreen : AppNavigationEvent
+    data object AppScreen : AppNavigationEvent
 }
