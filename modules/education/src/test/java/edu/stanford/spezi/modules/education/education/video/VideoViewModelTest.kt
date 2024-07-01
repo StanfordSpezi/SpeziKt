15eb63e1807b5843e8c64e5109536c70e70e79d1
Basler182 <showdialog@web.de>
2024-07-01 22:32:22 +0200

    fix detekt
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/education/src/test/java/edu/stanford/spezi/modules/education/education/video/VideoViewModelTest.kt
+++ modules/education/src/test/java/edu/stanford/spezi/modules/education/education/video/VideoViewModelTest.kt
@@ -1,6 +1,5 @@
 package edu.stanford.spezi.modules.education.education.video
 
-
 import edu.stanford.spezi.core.navigation.Navigator
 import edu.stanford.spezi.modules.education.EducationNavigationEvent
 import edu.stanford.spezi.modules.education.video.Action
@@ -10,12 +9,10 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
 
-
 class VideoViewModelTest {
 
     private val navigator: Navigator = mockk(relaxed = true)
     private var viewModel: VideoViewModel = VideoViewModel(navigator)
-
 
     @Test
     fun `when back pressed, navigate to pop up`() {
