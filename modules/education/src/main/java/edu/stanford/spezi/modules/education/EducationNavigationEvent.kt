a9a6b8f487009ddab440db210cf776073c975b9f
Basler182 <showdialog@web.de>
2024-07-05 11:45:24 +0200

    pass whole video to video detail screen
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/education/src/main/java/edu/stanford/spezi/modules/education/EducationNavigationEvent.kt
+++ modules/education/src/main/java/edu/stanford/spezi/modules/education/EducationNavigationEvent.kt
@@ -1,9 +1,10 @@
 package edu.stanford.spezi.modules.education
 
 import edu.stanford.spezi.core.navigation.NavigationEvent
+import edu.stanford.spezi.modules.education.videos.Video
 
 sealed class EducationNavigationEvent : NavigationEvent {
-    data class VideoSectionClicked(val youtubeId: String, val title: String) :
+    data class VideoSectionClicked(val video: Video) :
         EducationNavigationEvent()
 
     data object PopUp : EducationNavigationEvent()
