37f73c291b0fcf6e355dae8a402f1911b9a53478
Basler182 <showdialog@web.de>
2024-07-05 11:47:17 +0200

    added serialisation for navigation
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/education/build.gradle.kts
+++ modules/education/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.spezi.library)
     alias(libs.plugins.spezi.compose)
     alias(libs.plugins.spezi.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -13,6 +14,7 @@ dependencies {
     implementation(libs.androidyoutubeplayer.core)
     implementation(libs.coil.compose)
     implementation(libs.hilt.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(project(":core:navigation"))
 
