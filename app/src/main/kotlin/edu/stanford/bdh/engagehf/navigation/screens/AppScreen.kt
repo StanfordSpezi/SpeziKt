80a97f2ba41a6e830f60db22bc6b114053db9ea4
Eldi Cano <eldi.cano@hotmail.com>
2024-07-08 21:50:03 +0200

    fix main activity view model recreation

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/navigation/screens/AppScreen.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/navigation/screens/AppScreen.kt
@@ -12,12 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import edu.stanford.bdh.engagehf.Action
 import edu.stanford.bdh.engagehf.BottomBarItem
+import edu.stanford.bdh.engagehf.MainActivity
 import edu.stanford.bdh.engagehf.MainActivityViewModel
 import edu.stanford.bdh.engagehf.bluetooth.screen.BluetoothScreen
 import edu.stanford.bdh.engagehf.navigation.data.models.AppUiState
@@ -27,7 +29,9 @@ import edu.stanford.spezi.modules.education.videos.EducationScreen
 
 @Composable
 fun AppScreen() {
-    val viewModel = hiltViewModel<MainActivityViewModel>()
+    val viewModel = hiltViewModel<MainActivityViewModel>(
+        viewModelStoreOwner = LocalContext.current as MainActivity
+    )
     val uiState by viewModel.uiState.collectAsState()
     AppScreen(
         uiState = uiState,
