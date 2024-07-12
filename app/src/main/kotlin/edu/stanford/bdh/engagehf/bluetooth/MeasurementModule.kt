b4e44fd017b775baa728029bcff78883c140fdab
Basler182 <showdialog@web.de>
2024-07-12 20:15:35 +0200

    use @Binds instead of @Provides
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/bluetooth/MeasurementModule.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/bluetooth/MeasurementModule.kt
@@ -1,19 +1,16 @@
 package edu.stanford.bdh.engagehf.bluetooth
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.stanford.bdh.engagehf.bluetooth.data.mapper.DefaultMeasurementToRecordMapper
 import edu.stanford.bdh.engagehf.bluetooth.data.mapper.MeasurementToRecordMapper
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class MeasurementModule {
+abstract class MeasurementModule {
 
-    @Singleton
-    @Provides
-    fun provideMeasurementToRecordMapper(): MeasurementToRecordMapper =
-        DefaultMeasurementToRecordMapper()
+    @Binds
+    internal abstract fun bindMeasurementToRecordMapper(defaultMeasurementToRecordMapper: DefaultMeasurementToRecordMapper): MeasurementToRecordMapper
 }
