22974b3d478298b624d0fe2a5d04e4760396e7e2
Basler182 <showdialog@web.de>
2024-07-14 15:36:47 +0200

    added BottomSheetContent to UiState
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/navigation/data/models/AppUiState.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/navigation/data/models/AppUiState.kt
@@ -1,8 +1,11 @@
 package edu.stanford.bdh.engagehf.navigation.data.models
 
 import edu.stanford.bdh.engagehf.BottomBarItem
+import edu.stanford.bdh.engagehf.BottomSheetContent
 
 data class AppUiState(
     val items: List<BottomBarItem>,
     val selectedItem: BottomBarItem,
+    val isBottomSheetExpanded: Boolean = false,
+    val bottomSheetContent: BottomSheetContent? = null,
 )
