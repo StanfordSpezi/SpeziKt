79686409ed88fd3b06a87a1c127dd2b49e394b8e
Basler182 <showdialog@web.de>
2024-07-14 14:19:27 +0200

    added getVideoBySectionAndVideoId to EngageEducationRepository
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/education/EngageEducationRepository.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/education/EngageEducationRepository.kt
@@ -1,6 +1,7 @@
 package edu.stanford.bdh.engagehf.education
 
 import com.google.firebase.firestore.FirebaseFirestore
+import edu.stanford.spezi.modules.education.videos.Video
 import edu.stanford.spezi.modules.education.videos.VideoSection
 import edu.stanford.spezi.modules.education.videos.data.repository.EducationRepository
 import kotlinx.coroutines.tasks.await
@@ -15,6 +16,27 @@ class EngageEducationRepository @Inject constructor(
             firebaseFirestore.collection("videoSections").get().await().mapNotNull { document ->
                 mapper.map(document)
             }.sortedBy { it.orderIndex }
+        }
+    }
+
+    override suspend fun getVideoBySectionAndVideoId(
+        sectionId: String,
+        videoId: String,
+    ): Result<Video> {
+        return runCatching {
+            firebaseFirestore.collection("videoSections")
+                .document(sectionId)
+                .collection("videos")
+                .document(videoId)
+                .get()
+                .await().let { document ->
+                    if (document.exists()) {
+                        println(document.data)
+                        mapper.mapVideo(document)
+                    } else {
+                        throw IllegalStateException("Video not found")
+                    }
+                } ?: error("Video not found")
         }
     }
 }
--- modules/education/src/main/java/edu/stanford/spezi/modules/education/videos/data/repository/EducationRepository.kt
+++ modules/education/src/main/java/edu/stanford/spezi/modules/education/videos/data/repository/EducationRepository.kt
@@ -1,7 +1,10 @@
 package edu.stanford.spezi.modules.education.videos.data.repository
 
+import edu.stanford.spezi.modules.education.videos.Video
 import edu.stanford.spezi.modules.education.videos.VideoSection
 
 interface EducationRepository {
     suspend fun getVideoSections(): Result<List<VideoSection>>
+
+    suspend fun getVideoBySectionAndVideoId(sectionId: String, videoId: String): Result<Video>
 }
