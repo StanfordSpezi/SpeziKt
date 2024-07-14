397252191014c2ab8257df44b7518275ed69dafb
Basler182 <showdialog@web.de>
2024-07-14 14:23:34 +0200

    added placeholder screens for health and medication
    Signed-off-by: Basler182 <showdialog@web.de>

---
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/health/HealthScreen.kt
@@ -0,0 +1,9 @@
+package edu.stanford.bdh.engagehf.health
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HealthScreen() {
+    Text(text = "Health Screen")
+}
---
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/medication/MedicationScreen.kt
@@ -0,0 +1,9 @@
+package edu.stanford.bdh.engagehf.medication
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MedicationScreen() {
+    Text(text = "Medication Screen")
+}
