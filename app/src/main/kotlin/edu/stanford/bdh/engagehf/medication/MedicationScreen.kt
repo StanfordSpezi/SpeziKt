b43509f1534260ceca6c125b5f1b289c49ab4e3d
Basler182 <showdialog@web.de>
2024-08-04 20:40:54 +0200

    added MedicationScreenTestIdentifier
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/medication/MedicationScreen.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/medication/MedicationScreen.kt
@@ -1,51 +1,25 @@
 package edu.stanford.bdh.engagehf.medication
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.hilt.navigation.compose.hiltViewModel
-import edu.stanford.bdh.engagehf.medication.components.DosageInformation
 import edu.stanford.bdh.engagehf.medication.components.MedicationList
-import edu.stanford.bdh.engagehf.medication.components.MedicationProgressBar
-import edu.stanford.bdh.engagehf.medication.components.MedicationStatusIcon
 import edu.stanford.bdh.engagehf.medication.components.getMedicationDetailsByStatus
-import edu.stanford.spezi.core.design.component.VerticalSpacer
 import edu.stanford.spezi.core.design.theme.Colors
 import edu.stanford.spezi.core.design.theme.Colors.primary
-import edu.stanford.spezi.core.design.theme.Sizes
-import edu.stanford.spezi.core.design.theme.Spacings
 import edu.stanford.spezi.core.design.theme.SpeziTheme
 import edu.stanford.spezi.core.design.theme.TextStyles
 import edu.stanford.spezi.core.design.theme.ThemePreviews
-import edu.stanford.spezi.core.design.theme.lighten
+import edu.stanford.spezi.core.utils.extensions.testIdentifier
 
 @Composable
 fun MedicationScreen() {
@@ -65,20 +39,25 @@ fun MedicationScreen(
     when (uiState) {
         is MedicationUiState.Error -> {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testIdentifier(MedicationScreenTestIdentifier.ERROR_ROOT),
                 contentAlignment = Alignment.Center,
             ) {
                 Text(
                     text = (uiState).message,
                     color = Colors.error,
                     style = TextStyles.titleMedium,
+                    modifier = Modifier.testIdentifier(MedicationScreenTestIdentifier.ERROR_TEXT),
                 )
             }
         }
 
         MedicationUiState.Loading -> {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testIdentifier(MedicationScreenTestIdentifier.LOADING),
                 contentAlignment = Alignment.Center,
             ) {
                 CircularProgressIndicator(color = primary)
@@ -89,101 +68,8 @@ fun MedicationScreen(
             MedicationList(
                 uiState = uiState,
                 onAction = onAction,
+                modifier = Modifier.testIdentifier(MedicationScreenTestIdentifier.SUCCESS)
             )
-        }
-    }
-}
-
-@Composable
-fun MedicationCard(
-    modifier: Modifier = Modifier,
-    medicationDetails: MedicationDetails,
-    onAction: (MedicationViewModel.Action) -> Unit,
-) {
-    ElevatedCard(
-        elevation = CardDefaults.elevatedCardElevation(
-            defaultElevation = Sizes.Elevation.medium,
-        ),
-        colors = CardDefaults.cardColors(
-            containerColor = Colors.surface.lighten(isSystemInDarkTheme()),
-        ),
-        modifier = modifier,
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(Spacings.medium)
-                .animateContentSize()
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.clickable {
-                onAction(
-                    MedicationViewModel.Action.ExpandMedication(
-                        medicationId = medicationDetails.id,
-                        isExpanded = !medicationDetails.isExpanded,
-                    )
-                )
-            }) {
-                MedicationStatusIcon(medicationDetails = medicationDetails)
-                Spacer(modifier = Modifier.width(Spacings.small))
-                Column {
-                    Text(
-                        text = medicationDetails.title,
-                        style = TextStyles.titleLarge,
-                    )
-                    Text(
-                        text = medicationDetails.subtitle,
-                        style = TextStyles.titleSmall
-                    )
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                IconButton(onClick = {
-                    onAction(
-                        MedicationViewModel.Action.ExpandMedication(
-                            medicationDetails.id,
-                            !medicationDetails.isExpanded
-                        )
-                    )
-                }) {
-                    Icon(
-                        Icons.Default.ArrowDropDown,
-                        contentDescription = null,
-                    )
-                }
-            }
-            if (medicationDetails.isExpanded) {
-                if (medicationDetails.description.isNotEmpty()) {
-                    Spacer(modifier = Modifier.height(Spacings.small))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Text(
-                            text = medicationDetails.description,
-                            maxLines = Int.MAX_VALUE,
-                            overflow = TextOverflow.Clip,
-                            modifier = Modifier.weight(1f)
-                        )
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Filled.Info, contentDescription = "Information Icon")
-                        }
-                    }
-                }
-                if (medicationDetails.dosageInformation != null) {
-                    HorizontalDivider(
-                        modifier = Modifier.padding(
-                            top = Spacings.small,
-                            bottom = Spacings.small
-                        )
-                    )
-                    DosageInformation(dosageInformation = medicationDetails.dosageInformation)
-                    VerticalSpacer()
-                    MedicationProgressBar(
-                        currentProgress = medicationDetails.dosageInformation.currentDailyIntake.toFloat(),
-                        targetProgress = medicationDetails.dosageInformation.targetDailyIntake.toFloat()
-                    )
-                }
-            }
         }
     }
 }
@@ -210,6 +96,17 @@ private class UiStateProvider : PreviewParameterProvider<MedicationUiState> {
             )
         )
     )
+}
+
+enum class MedicationScreenTestIdentifier {
+    LOADING,
+    ERROR_ROOT,
+    ERROR_TEXT,
+    SUCCESS,
+    SUCCESS_MEDICATION_CARD_ROOT,
+    SUCCESS_MEDICATION_CARD_TITLE,
+    SUCCESS_MEDICATION_CARD_SUBTITLE,
+    SUCCESS_MEDICATION_CARD_DESCRIPTION,
 }
 
 @ThemePreviews
