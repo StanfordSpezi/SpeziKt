42aa938ca496c29212aa53533ff1ce19c1aabe95
Basler182 <showdialog@web.de>
2024-08-10 17:06:11 +0200

    added Symptoms ObservationCollection
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/observations/ObservationCollection.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/observations/ObservationCollection.kt
@@ -6,4 +6,5 @@ enum class ObservationCollection(val collectionName: String, val loinc: Loinc) {
     HEART_RATE(collectionName = "heartRateObservations", loinc = Loinc.HEART_RATE),
     BODY_WEIGHT(collectionName = "bodyWeightObservations", loinc = Loinc.WEIGHT),
     BLOOD_PRESSURE(collectionName = "bloodPressureObservations", loinc = Loinc.BLOOD_PRESSURE),
+    SYMPTOMS(collectionName = "symptomScores", Loinc.N_A),
 }
