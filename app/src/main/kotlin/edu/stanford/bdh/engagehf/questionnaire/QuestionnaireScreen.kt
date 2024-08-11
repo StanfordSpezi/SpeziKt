75c6392d66887c315f3e6820c5caf58a4f8a0d30
Basler182 <showdialog@web.de>
2024-08-11 16:04:20 +0200

    removed scroll in q screen
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/questionnaire/QuestionnaireScreen.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/questionnaire/QuestionnaireScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -61,7 +59,6 @@ private fun QuestionnaireLoaded(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
     ) {
         VerticalSpacer()
         Text(
