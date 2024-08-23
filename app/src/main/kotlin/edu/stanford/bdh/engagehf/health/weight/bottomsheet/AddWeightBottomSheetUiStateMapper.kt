5b8494884a1849d2f31953c47e141b7acf58f069
Basler182 <showdialog@web.de>
2024-08-23 15:29:50 +0200

    adjusted Add Weight data to BP and HR
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/health/weight/bottomsheet/AddWeightBottomSheetUiStateMapper.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/health/weight/bottomsheet/AddWeightBottomSheetUiStateMapper.kt
@@ -1,19 +1,57 @@
 package edu.stanford.bdh.engagehf.health.weight.bottomsheet
 
-import java.time.ZonedDateTime
+import edu.stanford.bdh.engagehf.health.DateTimeMapper
+import edu.stanford.bdh.engagehf.health.bloodpressure.bottomsheet.TimePickerState
+import edu.stanford.spezi.core.utils.LocaleProvider
+import java.time.LocalDate
+import java.time.LocalTime
 import javax.inject.Inject
 
-class AddWeightBottomSheetUiStateMapper @Inject constructor() {
+class AddWeightBottomSheetUiStateMapper @Inject constructor(
+    private val dateTimeMapper: DateTimeMapper,
+    private val localeProvider: LocaleProvider,
+) {
 
-    fun mapSaveWeightActionToUiState(
-        previousState: AddWeightBottomSheetViewModel.UiState,
-    ): AddWeightBottomSheetViewModel.UiState {
-        return previousState.copy(
-            weight = null,
-            selectedDateMillis = ZonedDateTime.now().toInstant().toEpochMilli(),
-            hour = ZonedDateTime.now().hour,
-            minute = ZonedDateTime.now().minute,
-            currentStep = AddWeightBottomSheetViewModel.Step.WEIGHT
+    fun mapInitialUiState(): AddWeightBottomSheetUiState {
+        val localDate = LocalDate.now()
+        val localTime = LocalTime.now()
+        return AddWeightBottomSheetUiState(
+            timePickerState = TimePickerState(
+                selectedDate = localDate,
+                selectedTime = localTime,
+                initialHour = localTime.hour,
+                initialMinute = localTime.minute,
+                selectedDateFormatted = dateTimeMapper.formatDate(localDate),
+                selectedTimeFormatted = dateTimeMapper.formatTime(localTime)
+            ),
+            weightUnit = when (localeProvider.getDefaultLocale().country) {
+                "US", "LR", "MM" -> WeightUnit.LBS
+                else -> WeightUnit.KG
+            }
+        )
+    }
+
+    fun mapUpdateDateAction(
+        date: LocalDate,
+        uiState: AddWeightBottomSheetUiState,
+    ): AddWeightBottomSheetUiState {
+        return uiState.copy(
+            timePickerState = uiState.timePickerState.copy(
+                selectedDate = date,
+                selectedDateFormatted = dateTimeMapper.formatDate(date)
+            )
+        )
+    }
+
+    fun mapUpdateTimeAction(
+        time: LocalTime,
+        uiState: AddWeightBottomSheetUiState,
+    ): AddWeightBottomSheetUiState {
+        return uiState.copy(
+            timePickerState = uiState.timePickerState.copy(
+                selectedTime = time,
+                selectedTimeFormatted = dateTimeMapper.formatTime(time)
+            )
         )
     }
 }
