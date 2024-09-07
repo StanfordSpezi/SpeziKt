4b465bbeadb5cf11152335f49eb70049d3e86edb
Basler182 <showdialog@web.de>
2024-09-07 20:43:48 +0200

    changed notification to String
    Signed-off-by: Basler182 <showdialog@web.de>

--- core/notification/src/main/java/edu/stanford/spezi/core/notification/notifier/NoOpNotifier.kt
+++ core/notification/src/main/java/edu/stanford/spezi/core/notification/notifier/NoOpNotifier.kt
@@ -1,11 +1,10 @@
 package edu.stanford.spezi.core.notification.notifier
 
-import com.google.firebase.messaging.RemoteMessage
 import javax.inject.Inject
 
 /**
  * A [NotificationNotifier] that does nothing. Useful for testing.
  */
 internal class NoOpNotifier @Inject constructor() : NotificationNotifier {
-    override fun sendNotification(remoteMessage: RemoteMessage) = Unit
+    override fun sendNotification(message: String) = Unit
 }
--- core/notification/src/main/java/edu/stanford/spezi/core/notification/notifier/NotificationNotifier.kt
+++ core/notification/src/main/java/edu/stanford/spezi/core/notification/notifier/NotificationNotifier.kt
@@ -1,10 +1,8 @@
 package edu.stanford.spezi.core.notification.notifier
 
-import com.google.firebase.messaging.RemoteMessage
-
 /**
  * Interface for sending notifications.
  */
 interface NotificationNotifier {
-    fun sendNotification(remoteMessage: RemoteMessage)
+    fun sendNotification(message: String)
 }
