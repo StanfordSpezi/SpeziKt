c564881e40c4ab2cb85eabf8163955a50b4cdee4
Basler182 <showdialog@web.de>
2024-09-14 22:32:27 +0200

    updated FCMNotificationService
    Signed-off-by: Basler182 <showdialog@web.de>

--- core/notification/src/main/java/edu/stanford/spezi/core/notification/fcm/FCMNotificationService.kt
+++ core/notification/src/main/java/edu/stanford/spezi/core/notification/fcm/FCMNotificationService.kt
@@ -5,6 +5,10 @@ import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import edu.stanford.spezi.core.coroutines.di.Dispatching
 import edu.stanford.spezi.core.logging.speziLogger
+import edu.stanford.spezi.core.notification.notifier.FirebaseMessage
+import edu.stanford.spezi.core.notification.notifier.FirebaseMessage.Companion.ACTION_KEY
+import edu.stanford.spezi.core.notification.notifier.FirebaseMessage.Companion.IS_DISMISSIBLE_KEY
+import edu.stanford.spezi.core.notification.notifier.FirebaseMessage.Companion.MESSAGE_ID_KEY
 import edu.stanford.spezi.core.notification.notifier.NotificationNotifier
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
@@ -32,12 +36,24 @@ class FCMNotificationService @Inject internal constructor(
     @Dispatching.IO
     lateinit var ioDispatcher: CoroutineDispatcher
 
+    /**
+     * Called when a message is received from Firebase Cloud Messaging.
+     * Example remoteMessage.data:
+     * {messageId=XYZID, action=observations, type=Vitals, isDismissible=true}
+     */
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        logger.i { "message received: $remoteMessage" }
-        remoteMessage.notification?.body?.let { body ->
-            notificationNotifier.sendNotification(body)
+        logger.i { "data: ${remoteMessage.data}" }
+        remoteMessage.notification?.let { notification ->
+            notificationNotifier.sendNotification(
+                firebaseMessage = FirebaseMessage(
+                    title = notification.title ?: "New ENGAGE-HF Message",
+                    message = notification.body ?: "Open the app to view the message",
+                    action = remoteMessage.data[ACTION_KEY],
+                    messageId = remoteMessage.data[MESSAGE_ID_KEY],
+                    isDismissible = remoteMessage.data[IS_DISMISSIBLE_KEY]?.toBoolean(),
+                )
+            )
         }
-        super.onMessageReceived(remoteMessage)
     }
 
     override fun onNewToken(token: String) {
