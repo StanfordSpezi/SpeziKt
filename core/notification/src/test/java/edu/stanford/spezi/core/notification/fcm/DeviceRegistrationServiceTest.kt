376202658e83923964c646b06c8ee9ac473b3fd9
Basler182 <showdialog@web.de>
2024-09-16 20:26:07 +0200

    adjusted DeviceRegistrationServiceTest
    Signed-off-by: Basler182 <showdialog@web.de>

--- core/notification/src/test/java/edu/stanford/spezi/core/notification/fcm/DeviceRegistrationServiceTest.kt
+++ core/notification/src/test/java/edu/stanford/spezi/core/notification/fcm/DeviceRegistrationServiceTest.kt
@@ -1,10 +1,13 @@
 package edu.stanford.spezi.core.notification.fcm
 
 import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import com.google.firebase.functions.FirebaseFunctions
-import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -12,29 +15,31 @@ class DeviceRegistrationServiceTest {
 
     private val context: Context = mockk(relaxed = true)
     private val functions: FirebaseFunctions = mockk(relaxed = true)
-    private val messageTokenService = mockk<MessageTokenService>(relaxed = true)
+    private val packageManager: PackageManager = mockk(relaxed = true)
+    private val packageInfo: PackageInfo = mockk(relaxed = true)
 
     private val service: DeviceRegistrationService =
         DeviceRegistrationService(
             context = context,
             functions = functions,
-            messageTokenService = messageTokenService,
+            ioDispatcher = UnconfinedTestDispatcher()
         )
 
     @Test
-    fun `registerDevice should not call register device function when notification token is null`() =
+    fun `registerDevice should call register device function when called`() =
         runTest {
-            // Arrange
-            val exception = Exception("Failed to fetch token")
-            coEvery { messageTokenService.getNotificationToken() } throws exception
+            // given
+            val token = "token"
+            packageInfo.versionName = "1.0.0"
+            packageInfo.versionCode = 1
+            every { context.packageManager } returns packageManager
+            every { context.packageName } returns "edu.stanford.spezi"
+            every { packageManager.getPackageInfo("edu.stanford.spezi", 0) } returns packageInfo
 
-            // When
-            service.registerDevice()
+            // when
+            service.registerDevice(token)
 
-            // Then
-            coVerify(
-                atMost = 0,
-                atLeast = 0,
-            ) { functions.getHttpsCallable(any()) }
+            // then
+            coVerify { functions.getHttpsCallable("registerDevice").call(any()) }
         }
 }
