a9db0b58967087172ab7b5d5549f619dd8ed34db
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-09-26 14:26:30 -0700

    Add new AccountDetails keys

---
+++ modules/account/src/main/kotlin/edu/stanford/spezi/module/account/firebase/account/AccountDetailsKeys.kt
@@ -0,0 +1,18 @@
+package edu.stanford.spezi.module.account.firebase.account
+
+import edu.stanford.spezi.module.account.account.AccountAnchor
+import edu.stanford.spezi.module.account.account.AccountDetails
+import edu.stanford.spezi.module.account.foundation.KnowledgeSource
+import java.util.Date
+
+private enum class CreationDateKey: KnowledgeSource<AccountAnchor, Date>
+
+var AccountDetails.creationDate: Date?
+    get() = this[CreationDateKey::class]
+    set(value) { this[CreationDateKey::class] = value }
+
+private enum class LastSignInDateKey: KnowledgeSource<AccountAnchor, Date>
+
+var AccountDetails.lastSignInDate: Date?
+    get() = this[LastSignInDateKey::class]
+    set(value) { this[LastSignInDateKey::class] = value }
