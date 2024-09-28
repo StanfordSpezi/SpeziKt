05740ef442fa62ef0106520e421dd123a001ae39
Basler182 <showdialog@web.de>
2024-09-28 13:17:13 +0200

    tested RegisterFormValidator
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/account/src/main/kotlin/edu/stanford/spezi/module/account/register/RegisterFormValidator.kt
+++ modules/account/src/main/kotlin/edu/stanford/spezi/module/account/register/RegisterFormValidator.kt
@@ -4,15 +4,9 @@ import javax.inject.Inject
 
 internal class RegisterFormValidator @Inject constructor() : FormValidator() {
 
-    fun firstnameResult(firstName: String): Result =
-        if (firstName.isNotEmpty()) Result.Valid else Result.Invalid("First name cannot be empty")
-
-    fun lastnameResult(lastName: String): Result =
-        if (lastName.isNotEmpty()) Result.Valid else Result.Invalid("Last name cannot be empty")
-
     fun isFormValid(uiState: RegisterUiState): Boolean {
         val passwordConditionSatisfied = {
-                isValidPassword(uiState.password.value).isValid
+            isValidPassword(uiState.password.value).isValid
         }
         return isValidEmail(uiState.email.value).isValid &&
             passwordConditionSatisfied()
