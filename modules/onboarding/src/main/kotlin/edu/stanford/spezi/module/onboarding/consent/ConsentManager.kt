6c4b7822bee6bce450ea815889ba99423a82c17c
Basler182 <showdialog@web.de>
2024-10-11 20:24:45 +0200

    doc ConsentManager
    Signed-off-by: Basler182 <showdialog@web.de>

--- modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/ConsentManager.kt
+++ modules/onboarding/src/main/kotlin/edu/stanford/spezi/module/onboarding/consent/ConsentManager.kt
@@ -1,5 +1,8 @@
 package edu.stanford.spezi.module.onboarding.consent
 
+/**
+ * A interface that needs to be implemented and provided by the app to provide the consent text and handle consent actions.
+ */
 interface ConsentManager {
     suspend fun getMarkdownText(): String
     suspend fun onConsented()
