c6f295b2a7f89b1e54bfb28c464e647bf4695798
Basler182 <showdialog@web.de>
2024-10-25 14:00:49 +0200

    unified heart health icons
    Signed-off-by: Basler182 <showdialog@web.de>

--- app/src/main/kotlin/edu/stanford/bdh/engagehf/health/symptoms/SymptomsScreen.kt
+++ app/src/main/kotlin/edu/stanford/bdh/engagehf/health/symptoms/SymptomsScreen.kt
@@ -1,6 +1,5 @@
 package edu.stanford.bdh.engagehf.health.symptoms
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,9 +11,9 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
@@ -31,9 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -71,7 +68,6 @@ import edu.stanford.bdh.engagehf.R
 import edu.stanford.bdh.engagehf.health.HealthTableItem
 import edu.stanford.spezi.core.design.component.CenteredBoxContent
 import edu.stanford.spezi.core.design.component.VerticalSpacer
-import edu.stanford.spezi.core.design.theme.Colors.onPrimary
 import edu.stanford.spezi.core.design.theme.Colors.primary
 import edu.stanford.spezi.core.design.theme.Colors.secondary
 import edu.stanford.spezi.core.design.theme.Sizes
@@ -163,14 +159,9 @@ private fun LazyListScope.listHeader(
                 onClick = { onAction(SymptomsViewModel.Action.Info) }
             ) {
                 Icon(
-                    painter = painterResource(id = edu.stanford.spezi.core.design.R.drawable.ic_info),
+                    imageVector = Icons.Filled.Info,
                     contentDescription = stringResource(R.string.info_icon_content_description),
-                    modifier = Modifier
-                        .size(Sizes.Icon.medium)
-                        .background(primary, shape = CircleShape)
-                        .shadow(Spacings.small, CircleShape)
-                        .padding(Spacings.small),
-                    tint = onPrimary
+                    tint = primary
                 )
             }
         }
