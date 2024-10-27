373134e858835b025ed34cf52b34fb2d4d1ff8e1
Basler182 <showdialog@web.de>
2024-10-27 19:13:01 +0100

    removed unused SemanticsMatcher.kt
    Signed-off-by: Basler182 <showdialog@web.de>

--- core/testing/src/main/kotlin/edu/stanford/spezi/core/testing/SemanticsMatcher.kt
+++
@@ -1,7 +0,0 @@
-package edu.stanford.spezi.core.testing
-
-import androidx.compose.ui.test.SemanticsMatcher
-import edu.stanford.spezi.core.utils.extensions.ImageResourceKey
-
-fun hasIconIdentifier(expectedIdentifier: String) =
-    SemanticsMatcher.expectValue(ImageResourceKey, expectedIdentifier)
