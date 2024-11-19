f3af7e3cdbea34839a1cfd80c830b72b0cb87725
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-11-19 12:54:54 -0800

    detekt

--- core/design/src/main/kotlin/edu/stanford/spezi/core/design/views/validation/views/VerifiableTextField.kt
+++ core/design/src/main/kotlin/edu/stanford/spezi/core/design/views/validation/views/VerifiableTextField.kt
@@ -1,6 +1,5 @@
 package edu.stanford.spezi.core.design.views.validation.views
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
