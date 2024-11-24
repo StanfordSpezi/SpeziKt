3ca3568713f2234b6e4b660d5798de94e24a4152
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-11-24 15:01:19 -0800

    Remove empty tests

--- modules/onboarding/src/androidTest/kotlin/edu/stanford/spezi/module/onboarding/spezi/composables/OverallOnboardingTestComposable.kt
+++
@@ -1,9 +0,0 @@
-package edu.stanford.spezi.module.onboarding.spezi.composables
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun OverallOnboardingTestComposable() {
-    Text("")
-}
