56d566733738277364a2d410b5e9ecdec40a35a7
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-11-24 13:03:42 -0800

    detekt

--- core/design/src/androidTest/kotlin/edu/stanford/spezi/core/design/questionnaire/simulators/QuestionnaireTestSimulator.kt
+++ core/design/src/androidTest/kotlin/edu/stanford/spezi/core/design/questionnaire/simulators/QuestionnaireTestSimulator.kt
@@ -3,5 +3,5 @@ package edu.stanford.spezi.core.design.questionnaire.simulators
 import androidx.compose.ui.test.junit4.ComposeTestRule
 
 data class QuestionnaireTestSimulator(
-    val composeTestRule: ComposeTestRule
+    val composeTestRule: ComposeTestRule,
 )
