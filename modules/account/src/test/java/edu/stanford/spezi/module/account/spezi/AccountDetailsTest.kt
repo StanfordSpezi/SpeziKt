c6a072dde00eb5d570002621ba54305ac1b43e55
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-11-26 16:34:00 -0800

    Add AccountDetailsTest

--- modules/account/src/test/java/edu/stanford/spezi/module/account/spezi/AccountDetailsTest.kt
+++ modules/account/src/test/java/edu/stanford/spezi/module/account/spezi/AccountDetailsTest.kt
@@ -2,11 +2,18 @@ package edu.stanford.spezi.module.account.spezi
 
 import com.google.common.truth.Truth.assertThat
 import edu.stanford.spezi.module.account.account.model.GenderIdentity
+import edu.stanford.spezi.module.account.account.service.configuration.AccountServiceConfiguration
+import edu.stanford.spezi.module.account.account.service.configuration.SupportedAccountKeys
+import edu.stanford.spezi.module.account.account.service.configuration.UserIdConfiguration
 import edu.stanford.spezi.module.account.account.value.AccountKeys
 import edu.stanford.spezi.module.account.account.value.collections.AccountDetails
 import edu.stanford.spezi.module.account.account.value.collections.AccountDetailsSerializer
+import edu.stanford.spezi.module.account.account.value.keys.AccountServiceConfigurationDetailsKey
+import edu.stanford.spezi.module.account.account.value.keys.accountId
+import edu.stanford.spezi.module.account.account.value.keys.email
 import edu.stanford.spezi.module.account.account.value.keys.genderIdentity
 import edu.stanford.spezi.module.account.account.value.keys.isNewUser
+import edu.stanford.spezi.module.account.account.value.keys.userId
 import kotlinx.serialization.json.Json
 import org.junit.Test
 
@@ -41,5 +48,35 @@ class AccountDetailsTest {
 
         assertThat(details.genderIdentity).isEqualTo(decoded.genderIdentity)
         assertThat(details.keys).isEqualTo(decoded.keys)
+    }
+
+    @Test
+    fun testUserIdKeyFallback() {
+        val details = AccountDetails()
+        details.accountId = "Hello World"
+        assertThat(details.userId).isEqualTo("Hello World")
+    }
+
+    @Test
+    fun testEmailKey() {
+        val details = AccountDetails()
+        details.userId = "username@example.org"
+        details[AccountServiceConfigurationDetailsKey] = AccountServiceConfiguration(
+            supportedKeys = SupportedAccountKeys.Arbitrary,
+            listOf(UserIdConfiguration.emailAddress)
+        )
+        details.email = "example@example.org"
+
+        assertThat(details.email).isEqualTo("example@example.org")
+        details.email = null
+        assertThat(details.email).isEqualTo("username@example.org")
+
+        val usernameDetails = AccountDetails()
+        usernameDetails.userId = "username"
+        usernameDetails[AccountServiceConfigurationDetailsKey] = AccountServiceConfiguration(
+            supportedKeys = SupportedAccountKeys.Arbitrary,
+            listOf(UserIdConfiguration.username),
+        )
+        assertThat(usernameDetails.email).isNull()
     }
 }
