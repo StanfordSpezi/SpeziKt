04d2145ba2c5ab3fdc85d2ef1774afa6eb88ad6a
Eldi Cano <eldi.cano@hotmail.com>
2025-02-16 19:01:44 +0100

    add number text field

--- heartbeat-app/src/main/kotlin/edu/stanford/bdh/heartbeat/app/survey/ui/fields/TextFieldItem.kt
+++ heartbeat-app/src/main/kotlin/edu/stanford/bdh/heartbeat/app/survey/ui/fields/TextFieldItem.kt
@@ -1,10 +1,12 @@
 package edu.stanford.bdh.heartbeat.app.survey.ui.fields
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import edu.stanford.bdh.heartbeat.app.survey.ui.QuestionFieldLabel
 import edu.stanford.bdh.heartbeat.app.survey.ui.QuestionNumberInfo
 import edu.stanford.bdh.heartbeat.app.survey.ui.SurveyCard
@@ -14,11 +16,17 @@ import edu.stanford.spezi.core.design.theme.ThemePreviews
 
 data class TextFieldItem(
     override val id: String,
+    val style: Style,
     val info: QuestionNumberInfo,
     val fieldLabel: QuestionFieldLabel,
     val value: String,
     val onValueChange: (String) -> Unit,
 ) : FormFieldItem {
+
+    enum class Style {
+        NUMERIC,
+        TEXT
+    }
 
     @Composable
     override fun Content(modifier: Modifier) {
@@ -27,8 +35,13 @@ data class TextFieldItem(
             fieldLabel.Content(Modifier)
             TextField(
                 value = value,
-                modifier = Modifier.bringIntoViewOnFocusedEvent().fillMaxWidth(),
-                onValueChange = { onValueChange(it) },
+                modifier = Modifier
+                    .bringIntoViewOnFocusedEvent()
+                    .fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = if (style == Style.NUMERIC) KeyboardType.Number else KeyboardType.Text
+                ),
+                onValueChange = onValueChange,
                 placeholder = { Text("Enter your input here...") }
             )
         }
@@ -37,6 +50,7 @@ data class TextFieldItem(
 
 val textFieldItem = TextFieldItem(
     id = "",
+    style = TextFieldItem.Style.NUMERIC,
     info = QuestionNumberInfo(2, 11),
     fieldLabel = QuestionFieldLabel("Phone number"),
     value = "",
