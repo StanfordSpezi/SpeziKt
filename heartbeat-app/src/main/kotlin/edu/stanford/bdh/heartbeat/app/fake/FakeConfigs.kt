8a7d6c731b438fffdb849b48fdfddf1ed4c8949d
Eldi Cano <eldi.cano@hotmail.com>
2025-02-18 00:06:43 +0100

    add fake configs docs

--- heartbeat-app/src/main/kotlin/edu/stanford/bdh/heartbeat/app/fake/FakeConfigs.kt
+++ heartbeat-app/src/main/kotlin/edu/stanford/bdh/heartbeat/app/fake/FakeConfigs.kt
@@ -1,9 +1,30 @@
 package edu.stanford.bdh.heartbeat.app.fake
 
+import edu.stanford.bdh.heartbeat.app.R
+
 object FakeConfigs {
+    /**
+     * If [true] simutales a fake flow with example questions from [R.raw.fake_data]
+     */
     const val ENABLED = true
+
+    /**
+     * Indicates whether a dummy login / register flow should be used, otherwise survey starts immediately
+     */
     const val SKIP_LOGIN = true
+
+    /**
+     * Max value in seconds that simulates async request durations to visualize loading states
+     */
     const val MAX_DELAY_SECONDS = 2L
-    const val EMAIL_VERIFIED = true
-    const val FORCE_ENABLE_CONTINUE = ENABLED
+
+    /**
+     * Whether dummy account should have the email already verified
+     */
+    const val EMAIL_VERIFIED = false
+
+    /**
+     * Set to true to ignore question validations and complete the flow
+     */
+    const val FORCE_ENABLE_CONTINUE = false
 }
