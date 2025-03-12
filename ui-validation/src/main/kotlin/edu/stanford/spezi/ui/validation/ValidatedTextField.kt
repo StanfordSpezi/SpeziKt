2ed6211c47b4df7f3325d4b50c8a530ea9c1a7b5
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2025-03-12 21:20:16 +0100

    update

--- ui-validation/src/main/kotlin/edu/stanford/spezi/ui/validation/ValidatedTextField.kt
+++ ui-validation/src/main/kotlin/edu/stanford/spezi/ui/validation/ValidatedTextField.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,7 +22,6 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
-import edu.stanford.spezi.ui.Button
 import edu.stanford.spezi.ui.Spacings
 import edu.stanford.spezi.ui.SpeziTheme
 import edu.stanford.spezi.ui.ThemePreviews
