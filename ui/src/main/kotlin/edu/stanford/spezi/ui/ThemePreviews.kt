952efc616351798ab4431a77311b1eb9681b30df
Eldi Cano <eldi.cano@hotmail.com>
2025-05-19 20:11:09 +0200

    remove old files

--- ui/src/main/kotlin/edu/stanford/spezi/ui/ThemePreviews.kt
+++
@@ -1,8 +0,0 @@
-package edu.stanford.spezi.ui
-
-import android.content.res.Configuration
-import androidx.compose.ui.tooling.preview.Preview
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme")
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme")
-annotation class ThemePreviews
